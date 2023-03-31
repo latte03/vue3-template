9f04ce890d80bf74506e0ce6561822fc89f89d7f
zengzp <1094143007@qq.com>
2023-03-31 15:05:46 +0800

    chore(deps): remove vite-plugin-pages
    undefined

--- types/shims.d.ts
+++ types/shims.d.ts
@@ -1,4 +1,3 @@
 /// <reference types="vite/client" />
-/// <reference types="vite-plugin-pages/client" />
 /// <reference types="vite-plugin-vue-layouts/client" />
 /// <reference types="unplugin-vue-macros/macros-global" />
