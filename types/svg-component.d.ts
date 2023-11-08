47694e69eb1324d9b1135e4872be2c0bedde9219
Agan <33367218+latte03@users.noreply.github.com>
2023-11-08 17:15:41 +0800

    fix: remove tsconfig.vitest.json

--- types/svg-component.d.ts
+++ types/svg-component.d.ts
@@ -1,37 +1,21 @@
+
 declare module '~virtual/svg-component' {
-  const SvgIcon: import('vue').DefineComponent<
-    {
+  const SvgIcon: import("vue").DefineComponent<{
       name: {
-        type: import('vue').PropType<'hot'>
-        default: string
-        required: true
-      }
-    },
-    {},
-    unknown,
-    {},
-    {},
-    import('vue').ComponentOptionsMixin,
-    import('vue').ComponentOptionsMixin,
-    {},
-    string,
-    import('vue').VNodeProps &
-      import('vue').AllowedComponentProps &
-      import('vue').ComponentCustomProps,
-    Readonly<
-      import('vue').ExtractPropTypes<{
-        name: {
-          type: import('vue').PropType<'hot'>
-          default: string
-          required: true
-        }
-      }>
-    >,
-    {
-      name: 'hot'
-    }
-  >
-  export const svgNames: ['hot']
-  export type SvgName = 'hot'
-  export default SvgIcon
+          type: import("vue").PropType<"hot">;
+          default: string;
+          required: true;
+      };
+  }, {}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {}, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{
+      name: {
+          type: import("vue").PropType<"hot">;
+          default: string;
+          required: true;
+      };
+  }>>, {
+      name: "hot";
+  }>;
+  export const svgNames: ["hot"];
+  export type SvgName = "hot";
+  export default SvgIcon;
 }
