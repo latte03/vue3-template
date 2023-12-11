307d85df63c16ac307a29a2b5218a75e4f42817d
曾泽坪 <1094143007@qq.com>
2023-12-11 16:26:04 +0800

    feat: 添加stylelint规则

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -4,25 +4,45 @@ module.exports = {
   extends: [
     'stylelint-config-standard',
     'stylelint-config-html/vue',
-    'stylelint-config-recess-order'
+    'stylelint-config-recess-order',
   ],
   customSyntax: 'postcss-html',
   ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
   overrides: [
     {
       files: ['*.less'],
-      customSyntax: 'postcss-less'
-    }
+      customSyntax: 'postcss-less',
+    },
   ],
   rules: {
     // https://github.com/AndyOGo/stylelint-declaration-strict-value
     'scale-unlimited/declaration-strict-value': [
       '/color$/',
       {
-        disableFix: true
+        disableFix: true,
         // https://github.com/AndyOGo/stylelint-declaration-strict-value#autofix-support
         // "autoFixFunc": "./auto-fix-func.js"
-      }
-    ]
-  }
+      },
+    ],
+    'property-no-vendor-prefix': null,
+    'scale-unlimited/declaration-strict-value': null,
+    'function-no-unknown': [
+      true,
+      {
+        ignoreFunctions: ['v-bind', 'deep'],
+      },
+    ],
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep', 'v-bind'],
+      },
+    ],
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['deep'],
+      },
+    ],
+  },
 }
